d2d0b32fe0d9781772d67977607716174cd3a1f5
Valerii Chystiakov <hnu.kn20.2.chistyakov@gmail.com>
2023-04-16 16:37:33 +0300

    adding-img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import photo from "./img/default-4.jpg";
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
     <Header />
     <h1>Hello</h1>
     <p>Hi</p>
+    <img src={photo} />
     <Footer />
   </div>)
   }
