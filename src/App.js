fa165efd7d619d12b50aa20388e9a726dad18dff
Valerii Chystiakov <hnu.kn20.2.chistyakov@gmail.com>
2023-04-17 14:52:05 +0300

    add-input-constructor-and-states

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import Footer from "./components/Footer";
 import photo from "./img/default-4.jpg";
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      helpText: 'Help mee!!'
+    }
+
+    this.inputClick = this.inputClick.bind(this)
+  }
+
   render() {
     return (<div className="name">
     <Header />
-    <h1>Hello</h1>
+    <h1>{this.state.helpText}</h1>
     <p>Hi</p>
-    <img src={photo} />
+    <input placeholder={this.state.helpText} onClick={this.inputClick}/>
+    <br/>
+    <img src={photo} alt={this.state.helpText} onClick={this.inputClick}/>
     <Footer />
   </div>)
+  }
+
+  inputClick(){
+    this.setState({ helpText: "aaaaaachello"})
   }
 }
 
